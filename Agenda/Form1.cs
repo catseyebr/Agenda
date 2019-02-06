b950604afbc76a8fa9abcf3e809d9575cacc695b
Eduardo Daniel Campelo de Assis <eduardoassis@outlook.com.br>
2019-02-06 18:26:27 -0200

    envio de arquivos

--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -10,11 +10,88 @@ using System.Windows.Forms;
 
 namespace Agenda
 {
-    public partial class Form1 : Form
+    public partial class frmAgendaContatos : Form
     {
-        public Form1()
+        public frmAgendaContatos()
         {
             InitializeComponent();
+        }
+
+        private void frmAgendaContatos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmAgendaContatos_Shown(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarCancelar(false);
+            AlterarBotoesIncluirAlterarExcluir(true);
+            CarregarListaContatos();
+        }
+
+        private void AlterarBotoesSalvarCancelar(bool estado)
+        {
+            btnSalvar.Enabled = estado;
+            btnCancelar.Enabled = estado;
+        }
+
+        private void AlterarBotoesIncluirAlterarExcluir(bool estado)
+        {
+            btnIncluir.Enabled = estado;
+            btnAlterar.Enabled = estado;
+            btnExcluir.Enabled = estado;
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesIncluirAlterarExcluir(false);
+            AlterarBotoesSalvarCancelar(true);
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesIncluirAlterarExcluir(false);
+            AlterarBotoesSalvarCancelar(true);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesIncluirAlterarExcluir(true);
+            AlterarBotoesSalvarCancelar(false);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Contato contato = new Contato
+            {
+                Nome = txbNome.Text,
+                Email = txbEmail.Text,
+                NumeroTelefone = txbNumeroTelefone.Text
+            };
+            List<Contato> contatosList = new List<Contato>();
+            foreach (Contato contatoLista in lbxContatos.Items)
+            {
+                contatosList.Add(contatoLista);
+            }
+            contatosList.Add(contato);
+            ManipuladorArquivos.EscreverArquivo(contatosList);
+            CarregarListaContatos();
+            AlterarBotoesSalvarCancelar(false);
+            AlterarBotoesIncluirAlterarExcluir(true);
+            LimparListaContatos();
+        }
+
+        private void CarregarListaContatos()
+        {
+            lbxContatos.Items.Clear();
+            lbxContatos.Items.AddRange(ManipuladorArquivos.LerArquivo().ToArray());
+        }
+
+        private void LimparListaContatos()
+        {
+            txbNome.Text = "";
+            txbEmail.Text = "";
+            txbNumeroTelefone.Text = "";
         }
     }
 }
